4816503c8286007c1122c969d7c3c464375bc534
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2021-01-04 09:56:28 +0100

    more on routing

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <Router>
-      <div className="App">
-        <header className="App-header">
-          <p>Building an ecomerce platform</p>
-        </header>
-      </div>
+      <Switch>
+        <div className="App">
+          <header className="App-header">
+            <p>Building an ecomerce platform</p>
+          </header>
+        </div>
+      </Switch>
     </Router>
   );
 }
