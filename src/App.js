0f3ebf9995a1f5ec0e1b5c12f78c03b75bddc03a
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2021-01-04 09:54:44 +0100

    adding routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Building an ecomerce platform</p>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <p>Building an ecomerce platform</p>
+        </header>
+      </div>
+    </Router>
   );
 }
 
