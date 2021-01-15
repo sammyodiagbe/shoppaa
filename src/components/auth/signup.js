fc0db1b86b983ce3e004042785eb68ba03b8638b
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2021-01-15 18:58:26 +0100

    more functionalities

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -2,40 +2,44 @@ import React, { useState } from "react";
 import "../../styles/pages/signup.css";
 import Buyer from "./buyer";
 import Seller from "./seller";
+import ChoosePlan from "../partials/select_plan";
 
 const Signup = () => {
   const [activeTab, setActiveTab] = useState(1);
   return (
-    <div className="signup-page">
-      <section className="left">
-        <div className="writeup">
-          <h1>Create your account</h1>
-          <p>
-            Either you want to sell your goods or your skills, could be arts or
-            your designs, pigeon makes it easy and straightforward.
-          </p>
-        </div>
-      </section>
-      <section className="right">
-        <div className="tab">
-          <button
-            className={`tab-btn buyer ${activeTab === 1 && "active"}`}
-            onClick={() => setActiveTab(1)}
-          >
-            I want to Buy
-          </button>
-          <button
-            className={`tab-btn buyer ${activeTab === 2 && "active"}`}
-            onClick={() => setActiveTab(2)}
-          >
-            I want to Sell
-          </button>
-        </div>
-        <div className="auth-body">
-          {activeTab === 1 ? <Buyer /> : <Seller />}
-        </div>
-      </section>
-    </div>
+    <React.Fragment>
+      <ChoosePlan />
+      <div className="signup-page">
+        <section className="left">
+          <div className="writeup">
+            <h1>Create your account</h1>
+            <p>
+              Either you want to sell your goods or your skills, could be arts
+              or your designs, pigeon makes it easy and straightforward.
+            </p>
+          </div>
+        </section>
+        <section className="right">
+          <div className="tab">
+            <button
+              className={`tab-btn buyer ${activeTab === 1 && "active"}`}
+              onClick={() => setActiveTab(1)}
+            >
+              I want to Buy
+            </button>
+            <button
+              className={`tab-btn buyer ${activeTab === 2 && "active"}`}
+              onClick={() => setActiveTab(2)}
+            >
+              I want to Sell
+            </button>
+          </div>
+          <div className="auth-body">
+            {activeTab === 1 ? <Buyer /> : <Seller />}
+          </div>
+        </section>
+      </div>
+    </React.Fragment>
   );
 };
 
--- src/components/partials/select_plan.js
+++ src/components/partials/select_plan.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ChoosePlan = () => {
-  return <div className="choose-plan">choose plan</div>;
+  const [plan, selectedPlan] = useState(1);
+  return (
+    <div className="choose-plan">
+      <div className="box">
+        <h1>Select a Plan</h1>
+        <div className="plans-container">
+          <section className={`plan ${plan === 1 ? "selected" : ""}`}>
+            <h1>Free</h1>
+            <button className="select-plan" onClick={() => selectedPlan(1)}>
+              {plan === 1 ? "Selected" : "Choose plan"}
+            </button>
+          </section>
+          <section className={`plan ${plan === 2 ? "selected" : ""}`}>
+            <h1>Mini</h1>
+            <button className="select-plan" onClick={() => selectedPlan(2)}>
+              {plan === 2 ? "Selected" : "Choose plan"}
+            </button>
+          </section>
+          <section className={`plan ${plan === 3 ? "selected" : ""}`}>
+            <h1>Maxi</h1>
+            <button className="select-plan" onClick={() => selectedPlan(3)}>
+              {plan === 3 ? "Selected" : "Choose plan"}
+            </button>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChoosePlan;
