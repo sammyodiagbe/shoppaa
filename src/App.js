58dcea182c1c19ef4e1a26d15d53943b74a47ca3
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2021-01-16 12:05:08 +0100

    implemented the plans page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LandingPage from "./components/pages/landing";
 import Signup from "./components/auth/signup";
+import ChoosePlan from "./components/partials/select_plan";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Switch>
           <Route path="/" exact component={LandingPage} />
           <Route path="/signup" exact component={Signup} />
+          <Route path="/choose_plan" exact component={ChoosePlan} />
         </Switch>
       </div>
     </Router>
--- src/components/partials/select_plan.js
+++ src/components/partials/select_plan.js
@@ -4,11 +4,16 @@ const ChoosePlan = () => {
   const [plan, selectedPlan] = useState(1);
   return (
     <div className="choose-plan">
-      <button className="close-dialog">&times;</button>
+      <a href="http://localhost:3000/dashboard" className="brand">
+        Cashew
+      </a>
+      <a href="http://localhost:3000/dashboard" className="close-dialog">
+        Send me to my Dashboard
+      </a>
       <div className="box">
-        <h1 className="almost-done">You are almost done</h1>
+        <h1 className="almost-done">You are almost done.</h1>
         <p className="choose-plan">
-          Choose a plan or start free then upgrade later
+          Choose a plan or start free then upgrade later.
         </p>
         <div className="plans-container">
           <section className={`plan ${plan === 1 ? "selected" : ""}`}>
@@ -16,6 +21,65 @@ const ChoosePlan = () => {
             <h2 className="price">
               0$ <span>/ month</span>
             </h2>
+            <div className="plan-offers">
+              <section className="column">
+                <div className="left">
+                  <p>Total store front</p>
+                </div>
+                <div className="right">
+                  {" "}
+                  <p className="total">1</p>
+                </div>
+              </section>
+              <section className="column">
+                <div className="left">
+                  <p>Photo per product</p>
+                </div>
+                <div className="right">
+                  <p className="total">1</p>
+                </div>
+              </section>
+              <section className="column">
+                <div className="left">
+                  <p>Real time chat</p>
+                </div>
+                <div className="right">
+                  <p className="total">yes</p>
+                </div>
+              </section>
+              <section className="column">
+                <div className="left">
+                  <p>Email alert</p>
+                </div>
+                <div className="right">
+                  <p className="total">yes</p>
+                </div>
+              </section>
+              <section className="column">
+                <div className="left">
+                  <p>SMS alert</p>
+                </div>
+                <div className="right">
+                  <p className="total">no</p>
+                </div>
+              </section>
+              <section className="column">
+                <div className="left">
+                  <p>Slashed Sales</p>
+                </div>
+                <div className="right">
+                  <p className="total">no</p>
+                </div>
+              </section>
+              <section className="column">
+                <div className="left">
+                  <p>Coupons</p>
+                </div>
+                <div className="right">
+                  <p className="total">no</p>
+                </div>
+              </section>
+            </div>
             <button className="select-plan" onClick={() => selectedPlan(1)}>
               {plan === 1 ? "Selected" : "Choose plan"}
             </button>
@@ -25,6 +89,65 @@ const ChoosePlan = () => {
             <h2 className="price">
               30$ <span>/ month</span>
             </h2>
+            <div className="plan-offers">
+              <section className="column">
+                <div className="left">
+                  <p>Total store front</p>
+                </div>
+                <div className="right">
+                  {" "}
+                  <p className="total">5</p>
+                </div>
+              </section>
+              <section className="column">
+                <div className="left">
+                  <p>Photo per product</p>
+                </div>
+                <div className="right">
+                  <p className="total">3</p>
+                </div>
+              </section>
+              <section className="column">
+                <div className="left">
+                  <p>Real time chat</p>
+                </div>
+                <div className="right">
+                  <p className="total">yes</p>
+                </div>
+              </section>
+              <section className="column">
+                <div className="left">
+                  <p>Email alert</p>
+                </div>
+                <div className="right">
+                  <p className="total">yes</p>
+                </div>
+              </section>
+              <section className="column">
+                <div className="left">
+                  <p>SMS alert</p>
+                </div>
+                <div className="right">
+                  <p className="total">yes</p>
+                </div>
+              </section>
+              <section className="column">
+                <div className="left">
+                  <p>Slashed Sales</p>
+                </div>
+                <div className="right">
+                  <p className="total">yes</p>
+                </div>
+              </section>
+              <section className="column">
+                <div className="left">
+                  <p>Coupons</p>
+                </div>
+                <div className="right">
+                  <p className="total">yes</p>
+                </div>
+              </section>
+            </div>
             <button className="select-plan" onClick={() => selectedPlan(2)}>
               {plan === 2 ? "Selected" : "Choose plan"}
             </button>
@@ -34,6 +157,64 @@ const ChoosePlan = () => {
             <h2 className="price">
               50$ <span>/ month</span>
             </h2>
+            <div className="plan-offers">
+              <section className="column">
+                <div className="left">
+                  <p>Total store front</p>
+                </div>
+                <div className="right">
+                  <p className="total">10</p>
+                </div>
+              </section>
+              <section className="column">
+                <div className="left">
+                  <p>Photo per product</p>
+                </div>
+                <div className="right">
+                  <p className="total">5</p>
+                </div>
+              </section>
+              <section className="column">
+                <div className="left">
+                  <p>Real time chat</p>
+                </div>
+                <div className="right">
+                  <p className="total">yes</p>
+                </div>
+              </section>
+              <section className="column">
+                <div className="left">
+                  <p>Email alert</p>
+                </div>
+                <div className="right">
+                  <p className="total">yes</p>
+                </div>
+              </section>
+              <section className="column">
+                <div className="left">
+                  <p>SMS alert</p>
+                </div>
+                <div className="right">
+                  <p className="total">yes</p>
+                </div>
+              </section>
+              <section className="column">
+                <div className="left">
+                  <p>Slashed Sales</p>
+                </div>
+                <div className="right">
+                  <p className="total">yes</p>
+                </div>
+              </section>
+              <section className="column">
+                <div className="left">
+                  <p>Coupons</p>
+                </div>
+                <div className="right">
+                  <p className="total">yes</p>
+                </div>
+              </section>
+            </div>
             <button className="select-plan" onClick={() => selectedPlan(3)}>
               {plan === 3 ? "Selected" : "Choose plan"}
             </button>
