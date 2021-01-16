6a6a8374ee3a25bf99db2344e27f88ffe29078ef
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2021-01-16 10:58:07 +0100

    more designs on the frontend

--- src/components/partials/select_plan.js
+++ src/components/partials/select_plan.js
@@ -4,23 +4,36 @@ const ChoosePlan = () => {
   const [plan, selectedPlan] = useState(1);
   return (
     <div className="choose-plan">
+      <button className="close-dialog">&times;</button>
       <div className="box">
-        <h1>Select a Plan</h1>
+        <h1 className="almost-done">You are almost done</h1>
+        <p className="choose-plan">
+          Choose a plan or start free then upgrade later
+        </p>
         <div className="plans-container">
           <section className={`plan ${plan === 1 ? "selected" : ""}`}>
-            <h1>Free</h1>
+            <h1 className="price-name">Free</h1>
+            <h2 className="price">
+              0$ <span>/ month</span>
+            </h2>
             <button className="select-plan" onClick={() => selectedPlan(1)}>
               {plan === 1 ? "Selected" : "Choose plan"}
             </button>
           </section>
           <section className={`plan ${plan === 2 ? "selected" : ""}`}>
-            <h1>Mini</h1>
+            <h1 className="price-name">Mini</h1>
+            <h2 className="price">
+              30$ <span>/ month</span>
+            </h2>
             <button className="select-plan" onClick={() => selectedPlan(2)}>
               {plan === 2 ? "Selected" : "Choose plan"}
             </button>
           </section>
           <section className={`plan ${plan === 3 ? "selected" : ""}`}>
-            <h1>Maxi</h1>
+            <h1 className="price-name">Maxi</h1>
+            <h2 className="price">
+              50$ <span>/ month</span>
+            </h2>
             <button className="select-plan" onClick={() => selectedPlan(3)}>
               {plan === 3 ? "Selected" : "Choose plan"}
             </button>
